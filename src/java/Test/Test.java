a633b392608293b17bfcdfc167e66d0ec1bdeefe
A-laili <asmaalaili18@gmail.com>
2023-10-24 19:50:57 +0100

    second commit

---
+++ src/java/Test/Test.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import dao.IDao;
+import entities.Machine;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.Hibernate;
+import service.MachineService;
+import util.HibernateUtil;
+
+/**
+ *
+ * @author HP
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        IDao<Machine> dao = new MachineService();
+        dao.create(new Machine("latitude", "DELL", 200));
+        dao.create(new Machine("thinkpad", "Lenovo", 300));
+        dao.create(new Machine("elitebook", "HP", 400));
+
+        for (Machine m : dao.findAll()) {
+            System.out.println(m);
+        }
+
+    }
+}
+
